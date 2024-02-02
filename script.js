c4d922235e6bbf8ad3cb934df22573b8c25b822e
Henkka <henri.anttonen04@hotmail.com>
2024-02-02 12:43:53 +0200

    Lightmode

---
+++ script.js
@@ -0,0 +1,14 @@
+// Function to switch to light mode
+function switchToLightMode() {
+    // Remove the 'dark-mode' class from the body
+    document.body.classList.remove('dark-mode');
+    
+    // Save the user's preference to local storage
+    localStorage.setItem('darkMode', false);
+}
+
+// Event listener for light mode button
+document.getElementById('light-mode-btn').addEventListener('click', function() {
+    // Switch to light mode
+    switchToLightMode();
+});
